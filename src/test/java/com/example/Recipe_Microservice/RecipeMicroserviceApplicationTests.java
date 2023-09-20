586599cd27c2187b1670d019e3c0ef9c84d412ea
aishu136 <aishu.aishu34@gmail.com>
2023-09-20 11:53:31 +0530

    first commit

---
+++ src/test/java/com/example/Recipe_Microservice/RecipeMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Recipe_Microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
